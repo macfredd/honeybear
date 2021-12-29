668eabec742b0c8d41953cbcf88ecb76ba97f55d
Freddy M Cruz Estrada <freddy.cruze@gmail.com>
2021-12-28 22:19:31 -0600

    API: Adding swagger API doc and Plugin

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -8,9 +9,19 @@ async function bootstrap() {
     new ValidationPipe(
       {
         whitelist: true,
-      }
-    )
-  )
+      },
+    ),
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('HoneyBear')
+    .setDescription('API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+
   await app.listen(3000);
 }
 bootstrap();
